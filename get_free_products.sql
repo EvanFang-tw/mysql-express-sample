59e03dd9ccdefab482ed610beeddfbf80678b4c1
evanfang <evanfang@moneysq.com>
2020-04-20 15:07:00 +0800

    Fix white space

--- get_free_products.sql
+++ get_free_products.sql
@@ -1,13 +1,13 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `get_free_products`(min_price INT)
 BEGIN
 
-	select 
-		id, 
+  select
+    id, 
     name, 
     remark, 
     price,
     if(price < min_price, 'Yes', 'No') as is_free
-	from 
-		products;
+  from 
+    products;
 
 END
